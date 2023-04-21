2f13c560f4e316de456718bb22ca0c357de8ad55
MattsSalv <m.salvatti@studenti.unibs.it>
2023-04-21 16:40:20 +0200

    Upload game

--- flappy_bird.py
+++ flappy_bird.py
@@ -6,26 +6,26 @@ import random
 pygame.font.init()
 
 
-WIN_WIDTH = 500             #Dimensione delle finestra di gioco
-WIN_HEIGHT = 600
+WIN_WIDTH = 600             #Dimensione delle finestra di gioco
+WIN_HEIGHT = 800
 
 #Nelle righe sottostanti vengono importante e ridemsnionate le immagini di gioco
 BIRD_IMGS =  [pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bird1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bird2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bird3.png")))]
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("images", "pipe.png")))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("images", "base.png")))  
-BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("images", "bg.png")))
+BG_IMG = pygame.transform.scale(pygame.image.load(os.path.join("images", "bg.png")), (600, 900))
 
-STAT_FONT = pygame.font.SysFont("comicsans", 50)
+STAT_FONT = pygame.font.SysFont("comicsans", 50) #CAMBIARE FONT
 
 
 class Bird:
     IMGS = BIRD_IMGS
     MAX_ROTATION = 25
     ROT_VEL = 20
-    ANIMATION_TIME = 5
+    ANIMATION_TIME = 5 #Ogni quanto cambia il frame e l'uccellino muove le ali
 
-    def __init__(self, x, y):
-        self.x = x  # coordinate iniziali
+    def __init__(self, x, y):    #Iniizializzazione dell'uccellino
+        self.x = x  
         self.y = y
         self.tilt = 0
         self.tick_count = 0
@@ -59,7 +59,7 @@ class Bird:
             if self.tilt > -90:
                 self.tilt -= self.ROT_VEL  
 
-    def draw(self, win):  #
+    def draw(self, win): 
         self.img_count += 1
 
         if self.img_count < self.ANIMATION_TIME:
@@ -92,17 +92,17 @@ class Pipe:
     def __init__(self, x):
         self.x = x
         self.height = 0
-        self.gap = 100
+        #self.gap = 100
 
         self.top = 0
         self.bottom = 0
-        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True) #Crea il tubo al contrario
         self.PIPE_BOTTOM = PIPE_IMG
 
         self.passed=False
         self.set_height()
 
-    def set_height(self):   
+    def set_height(self):   #Gestisce l'altezza dei tubi
         self.height = random.randrange(50, 450)
         self.top = self.height - self.PIPE_TOP.get_height()
         self.bottom = self.height + self.GAP
@@ -114,7 +114,7 @@ class Pipe:
         win.blit(self.PIPE_TOP, (self.x, self.top))   
         win.blit(self.PIPE_BOTTOM, (self.x, self.bottom)) 
 
-    def collide(self, bird):
+    def collide(self, bird):   #Le collisioni vengono gestite con le maschere della libreria pygame 
         bird_mask = bird.get_mask()
         top_mask = pygame.mask.from_surface(self.PIPE_TOP)  
         bottom_mask =  pygame.mask.from_surface(self.PIPE_BOTTOM)
@@ -131,7 +131,7 @@ class Pipe:
         return False
 
 class Base:
-    VEL = 5
+    VEL = 4
     WIDTH = BASE_IMG.get_width()
     IMG = BASE_IMG
 
@@ -145,7 +145,7 @@ class Base:
         self.x2 -= self.VEL
 
         if self.x1 + self.WIDTH < 0:
-            self.x1 = self.x2
+            self.x1 = self.x2 + self.WIDTH
 
         if self.x2 + self.WIDTH < 0:
             self.x2 = self.x1 + self.WIDTH  
@@ -154,24 +154,43 @@ class Base:
         win.blit(self.IMG, (self.x1, self.y))   
         win.blit(self.IMG, (self.x2, self.y))           
 
-def draw_window(win, bird, pipes, base, score):
-    win.blit(BG_IMG, (0,-250))
+def draw_window(win, birds, pipes, base, score):
+
+    win.blit(BG_IMG, (0,0))
 
     for pipe in pipes:
         pipe.draw(win)
 
-    text= STAT_FONT.render("Score " + str(score), 1, (255, 255, 255))   
+    text = STAT_FONT.render("Score " + str(score), 1, (255, 255, 255))   
     win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10)) 
 
     base.draw(win)    
 
-    bird.draw(win)
+    for bird in birds:
+        bird.draw(win)
+
     pygame.display.update()
 
-def main():   
-    bird = Bird(230, 350)
-    base = Base(500)
-    pipes = [Pipe(400)]
+
+#Fitness function
+def main(genomes, config):  
+
+    nets = []
+    ge = []
+    birds = []
+
+    #Creazione della rete neurale per ogni genomaD
+    for _, g in genomes:  
+        g.fitness = 0 #Setto il valore del fitness di ogni genoma a 0
+        net = neat.nn.FeedForwardNetwork.create(g, config) #creazione della rete neurale, passandogli il genoma e il file di configurazioned
+        nets.append(net)
+        birds.append(Bird(230, 350)) #creazione dell'uccellino relativo al genoma
+        ge.append(g)
+
+
+
+    base = Base(730)
+    pipes = [Pipe(700)]
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))  #creazione della fineastra di gioco
     clock = pygame.time.Clock()
 
@@ -179,44 +198,97 @@ def main():
 
     run = True
 
-    while run:   #serve per gestire la chiusura della finestra di gioco
-        clock.tick(30)
-        for event in pygame.event.get():   
-            if event.type == pygame.QUIT:
-                run = False
+    while run:
+            clock.tick(30) #Gestisce il framerate(rallenta il gioco)
+            for event in pygame.event.get():  #Si occupa di gestire la chiusura della finestra di gioco
+                if event.type == pygame.QUIT:
+                    run = False
+                    pygame.quit()
+                    quit()
 
-            #bird.move()
+            pipe_ind = 0
+            if len(birds) > 0:
+                if len(pipes) > 1 and birds[0].x > pipes[0].x + pipes[0].PIPE_TOP.get_width(): #RIGUARDARE
+                    pipe_ind = 1
+ 
+            else:   #Se non ho piu uccelli lascio il gioco
+                run = False
+                break  
+
+            #Per ogni frame che l'uccellino avanza viene aumentato il fitness di 0.1
+            for x, bird in enumerate(birds):
+                bird.move()   
+                ge[x].fitness += 0.1
+
+                output = nets[x].activate((bird.y, abs(bird.y - pipes[pipe_ind].height), abs(bird.y - pipes[pipe_ind].bottom))) #Attiviamo la rete neurale passandogli i valori(Ritorna un array)
+                if output[0] > 0.5: #Posso mettere 0 perchè ho solo un neurone di uscita
+                    bird.jump()
+
+
+
+            base.move()
             add_pipe = False
             rem = []
+
             for pipe in pipes:
-                if pipe.collide(bird):
-                    pass
-
-                if pipe.x + pipe.PIPE_TOP.get_width() < 0:
-                    rem.append(pipe)
-
-                if not pipe.passed and pipe.x < bird.x:
-                    pipe.passed = True    
-                    add_pipe = True
 
                 pipe.move()
 
-            if add_pipe:    
+                for x,bird in enumerate(birds): #Gestisce le collisioni di ogni uccellino con i tubi, si occupa di rimuove gli uccellini che collidono
+                    if pipe.collide(bird):
+                        ge[x].fitness -= 1 #il fitness dell'uccellino che ha colpito il tubo viene decrementato a 1 per non influenzare gli altri
+                        birds.pop(x)
+                        nets.pop(x)
+                        ge.pop(x)
+
+                    if not pipe.passed and pipe.x < bird.x: #Controlla che se il singolo uccellino ha passato il tubo
+                        pipe.passed = True    
+                        add_pipe = True
+            
+                if pipe.x + pipe.PIPE_TOP.get_width() < 0: #Si occupa di rimuovere il tubo una volta che l'uccellino lo ha superato
+                    rem.append(pipe)    
+
+            if add_pipe:   #Se supera i tubi, il punteggio viene incrementato e il valore di fitness dell'uccellino viene incrementato di 5 
                 score +=1
-                pipes.append(Pipe(700))
+                for g in ge:
+                    g.fitness += 5
+                pipes.append(Pipe(600)) #Aggiunge un nuovo pipe dopo che lo si è superato
 
             for r in rem:
                 pipes.remove(r)
 
-            if bird.y + bird.img.get_height() >= 730:
-                pass        
-
-
+            for x, bird in enumerate(birds): #Controlla le collissioni del singolo uccellino con il suolo e che non vada oltre la parte superiore dello schermo
+                if bird.y + bird.img.get_height() - 10 >= 730 or bird.y < -50:
+                    birds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)    
+                
             base.move()
-            draw_window(win, bird, pipes, base, score)    
-
-    pygame.quit()
-    quit()
+            draw_window(win, birds, pipes, base, score) #Crea i vari oggetti sullo schermo   
 
 
-main()    
+
+def run(config_path):
+
+    #Viene passato il path del file di configurazione e venogno settate le diverse proprietà
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+
+    #Creazione della popolazione
+    p = neat.Population(config) 
+
+    #Opzionale: serve per visualizzare nella console le statistche
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    #configurazione della fitness function, 50 è il numero di volte che chiamerà la funzione main(numero massimo di generazioni)
+    winner = p.run(main,50)
+    
+
+
+if __name__ == "__main__":
+
+    #Caricamento del file di configurazione
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config.txt")
+    run(config_path)
