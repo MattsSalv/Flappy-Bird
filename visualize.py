35eb24711914d58d10637a35776ba2729dd1052e
Claudio Onorati <130538194+ClaudioOnorati@users.noreply.github.com>
2023-05-02 09:28:51 +0200

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -3,14 +3,14 @@ try:
     import biggles # Requires biggles: http://biggles.sourceforge.net/
     has_biggles = True
 except ImportError:
-    print "Biggles is not installed. If you wish to automatically plot some nice statistics please install it: http://biggles.sourceforge.net/"
+    print ("Biggles is not installed. If you wish to automatically plot some nice statistics please install it: http://biggles.sourceforge.net/")
     has_biggles = False
 
 try:
     import pydot   # Requires PyDot: http://code.google.com/p/pydot/downloads/list
     has_pydot = True
 except:
-    print "PyDot is not installed. If you wish to generate a graphical representation of the resulting neural network, please install it: http://code.google.com/p/pydot/"
+    print ("PyDot is not installed. If you wish to generate a graphical representation of the resulting neural network, please install it: http://code.google.com/p/pydot/")
     has_pydot = False
 
 import random
@@ -43,7 +43,7 @@ def draw_net(chromosome, id=''):
         g = pydot.graph_from_dot_data(output)
         g.write('phenotype'+id+'.svg', prog='dot', format='svg')
     else:
-        print 'You do not have the PyDot package.'
+        print ('You do not have the PyDot package.')
 
 def draw_ff(chromosome):
     ''' Draws a feedforward neural network '''
@@ -74,7 +74,7 @@ def draw_ff(chromosome):
         g = pydot.graph_from_dot_data(output)
         g.write('feedforward.svg', prog='dot', format='svg')
     else:
-        print 'You do not have the PyDot package.'
+        print ('You do not have the PyDot package.')
 
 def plot_stats(stats):
     ''' Plots the population's average and best fitness. '''
@@ -96,7 +96,7 @@ def plot_stats(stats):
         plot.write_img(600, 300, 'avg_fitness.svg')
         # width and height doesn't seem to affect the output! 
     else:
-        print 'You dot not have the Biggles package.'
+        print ('You dot not have the Biggles package.')
 
 def plot_spikes(spikes):
     ''' Plots the trains for a single spiking neuron. '''
@@ -112,7 +112,7 @@ def plot_spikes(spikes):
         plot.write_img(600, 300, 'spiking_neuron.svg')
         # width and height doesn't seem to affect the output!
     else:
-        print 'You dot not have the Biggles package.'
+        print ('You dot not have the Biggles package.')
 
 def plot_species(species_log):
     ''' Visualizes speciation throughout evolution. '''
@@ -149,4 +149,4 @@ def plot_species(species_log):
         plot.write_img(1024, 800, 'speciation.svg')
 
     else:
-        print 'You dot not have the Biggles package.'
+        print ('You dot not have the Biggles package.')
